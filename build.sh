819d719fa3e82ce628e1948d772843b33922e8ee
nielsmommen <r0627548@kuleuven.be>
2021-10-05 09:36:38 +0200

    C++ deps, upgrade to OCaml 4.13 and upgrade deps to recent versions

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -x # Print what is being executed.
 pwd
 VFDEPS_VERSION=`git describe --always`
 VFDEPS_DIRNAME=vfdeps
+MSVC_INSTALL_DIR=${MSVC_INSTALL_DIR:-"C:/vfMinVS"}
 
 BUILD_DIR=`pwd`
 mkdir upload
@@ -17,7 +18,7 @@ VFDEPS_PLATFORM=win
 VFDEPS_DIR=$VFDEPS_PARENT_DIR/$VFDEPS_DIRNAME
 mkdir $VFDEPS_DIR
 
-/c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=$VFDEPS_DIR"
+/c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=\"$VFDEPS_DIR\" MSVC_INSTALL_DIR=\"$MSVC_INSTALL_DIR\""
 
 VFDEPS_FILENAME=$VFDEPS_DIRNAME-$VFDEPS_VERSION-$VFDEPS_PLATFORM-noz3.txz
 VFDEPS_FILEPATH=$UPLOAD_DIR/$VFDEPS_FILENAME
@@ -25,19 +26,5 @@ cd $VFDEPS_PARENT_DIR
 tar cjf $VFDEPS_FILEPATH $VFDEPS_DIRNAME
 cd $BUILD_DIR
 
-echo '{' > bintray.json
-echo '    "package": {' >> bintray.json
-echo '        "name": "vfdeps",' >> bintray.json
-echo '        "repo": "verifast",' >> bintray.json
-echo '        "subject": "verifast",' >> bintray.json
-echo '        "vcs_url": "https://github.com/verifast/vfdeps",' >> bintray.json
-echo '        "licenses": ["MIT"]' >> bintray.json
-echo '    },' >> bintray.json
-echo '    "version": {' >> bintray.json
-echo '        "name": "'$VFDEPS_VERSION'"' >> bintray.json
-echo '    },' >> bintray.json
-echo '    "files": [{"includePattern": "upload/(.*)", "uploadPattern": "$1"}],' >> bintray.json
-echo '    "publish": true' >> bintray.json
-echo '}' >> bintray.json
-
-cat bintray.json
+ls -l $VFDEPS_FILEPATH
+/c/cygwin/bin/bash -lc "sha224sum /cygdrive$VFDEPS_FILEPATH"
